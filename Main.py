4c2e325448ce6a3f1afda7d9717d9994bd978b46
Double X Li <bajnok33@gmail.com>
2016-12-07 20:33:48 +0800

    Finish Upload Part Almostly

--- Main.py
+++ Main.py
@@ -1,128 +1,186 @@
-import requests
+import os
+import sys
+from tkinter import Tk
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QButtonGroup
+from PyQt5.QtWidgets import QDesktopWidget
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QListWidget
+from PyQt5.QtWidgets import QListWidgetItem
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QRadioButton
+from PyQt5.QtWidgets import QTextEdit
+from PyQt5.QtWidgets import QWidget
+
+from SMMSTool import SMMSTool, ImageUrl
 
 
-class ImageUrl():
-    """
-    存放图片的路径和删除路径
-    """
-    def __init__(self, url, delete):
-        self.url = url
-        self.delete = delete
-
-
-class SMMSTool():
-    """
-    sm.ms工具
-    api：https://sm.ms/doc/
-    """
-    uploadUrl = 'https://sm.ms/api/upload'
-    historyUrl = 'https://sm.ms/api/list'
-    clearUrl = 'https://sm.ms/api/clear'
-    urlList = []
-
+class SMMSToolGUI(QWidget):
     def __init__(self):
-        self.cookies = dict(PHPSESSID='')
+        super().__init__()
+        self.initUI()
+        self.tool = SMMSTool()
+        self.hasResult = False
+        self.uploadList = []
 
-    def upload(self, filepath):
-        """
-        上传图片
-        :param filepath: 待上传图片路径
-        :return: [int, string] 状态码，状态
-        """
-        print("开始上传图片")
-        files = {
-            'smfile':open(filepath, 'rb')
-        }
-        r = requests.post(self.uploadUrl, files=files)
-        commit_data = r.json()
-        self.cookies['PHPSESSID'] = r.cookies['PHPSESSID']
+    def initUI(self):
+        grid = QGridLayout()
+        grid.setSpacing(10)
 
-        # 获取上传文件状态
-        code = commit_data['code']
-        if code == "success":       # 成功则构造ImageUrl加入urlList
-            url = commit_data['data']['url']
-            delete = commit_data['data']['delete']
-            self.urlList.append(ImageUrl(url, delete))
-            return [0, "上传成功"]
-        elif code == "error":       # 返回"error"则返回错误msg
-            msg = commit_data['msg']
-            return [-1, msg]
+        self.dragArea = QTextEdit()
+
+        self.fileinfo = QLineEdit()
+        self.removeBtn = QPushButton("Remove")
+        self.removeBtn.clicked.connect(self.clearUploadList)
+        self.removeBtn.hide()
+        self.uploadBtn = QPushButton("Upload")
+        self.uploadBtn.clicked.connect(self.upload)
+        self.uploadBtn.hide()
+        self.browseBtn = QPushButton("Browse...")
+        self.browseBtn.clicked.connect(self.browse)
+
+        self.label = QLabel()
+        self.getUrl = QRadioButton("url")
+        self.getUrl.clicked.connect(self.showUrl)
+        self.getMarkdown = QRadioButton("markdown")
+        self.getMarkdown.clicked.connect(self.showUrl)
+        self.getDelete = QRadioButton("delete")
+        self.getDelete.clicked.connect(self.showUrl)
+        self.radioGroup = QButtonGroup()
+        self.radioGroup.addButton(self.getUrl)
+        self.radioGroup.addButton(self.getMarkdown)
+        self.radioGroup.addButton(self.getDelete)
+
+        self.list = QListWidget()
+        self.list.itemClicked.connect(self.copyToClipboard)
+
+        grid.addWidget(self.dragArea, 1, 0, 1, 4)
+        grid.addWidget(self.fileinfo, 2, 0)
+        grid.addWidget(self.removeBtn, 2, 1)
+        grid.addWidget(self.uploadBtn, 2, 2)
+        grid.addWidget(self.browseBtn, 2, 3)
+        grid.addWidget(self.label, 3, 0, 1, 4)
+        grid.addWidget(self.getUrl, 4, 0)
+        grid.addWidget(self.getMarkdown, 4, 1)
+        grid.addWidget(self.getDelete, 4, 2)
+        grid.addWidget(self.list, 5, 0, 1, 4)
+
+        self.setLayout(grid)
+
+        self.setGeometry(300, 300, 500, 300)
+        self.center()
+        self.setWindowTitle('SMMSToolGUI')
+
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, '警告', "是否要退出?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
         else:
-            return [-2, "未知异常"]
+            event.ignore()
 
-    def delete(self, imageUrl):
-        """
-        删除图片
-        :param imageUrl: 待上传图片的ImageUrl对象
-        :return: [int, string] 状态码，状态
-        """
-        print("开始删除图片")
-        deleteUrl = imageUrl.delete
-        r = requests.get(deleteUrl)
-        if r.status_code == 200:
-            return [0, "删除成功"]
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def fileIsImage(self, filepath):
+        imageExtension = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
+        inputExtension = os.path.splitext(filepath)[1]
+        if inputExtension in imageExtension:
+            return True
         else:
-            return [-1, "未知异常"]
+            return False
 
-    def history(self):
-        """
-        查询上传历史
-        重要！！！：若要关闭程序后仍能查询到历史记录，应在upload中保存cookies，待施工
-        :return: [int, string] 状态码，状态
-        """
-        print("开始查询历史记录")
-        r = requests.get(self.historyUrl, cookies=self.cookies)
-        commit_data = r.json()
-        code = commit_data['code']
-        if code == 'success':
-            if len(commit_data['data']) == 0:
-                return [-1, "没有已上传的文件"]
+    def upload(self):
+        print("待传图片有" + len(self.uploadList).__str__())
+        for filepath in self.uploadList:
+            print("正在处理uploadList")
+            if os.path.isfile(filepath):
+                if self.fileIsImage(filepath):
+                    uploadStatus, uploadMsg = self.tool.upload(filepath)
+                    if uploadStatus == -1 or -2:
+                        print("Status:")
+                        print(uploadStatus)
+                    elif uploadStatus == 0:
+                        print("Status:")
+                        print(uploadStatus)
+                        print("上传成功时查看信息")
+                        self.tool.outputInfo()
+                else:
+                    print("不是图片")
             else:
-                return [0, commit_data['data']]
-        elif code == 'error':
-            return [-2, "没有已上传的文件"]
+                print("不是文件")
+        self.label.setText("上传成功")
+        self.hasResult = True
 
-    def clear(self):
-        """
-        清空历史记录
-        重要！！！：若要关闭程序后仍能清除历史记录，应在upload中保存cookies，待施工
-        :return: [str, str] 状态码，状态
-        """
-        print("开始清除历史记录")
-        r = requests.get(self.clearUrl, cookies = self.cookies)
-        commit_data = r.json()
-        code = commit_data['code']
-        msg = commit_data['msg']
-        return [code, msg]
+    def clearUploadList(self):
+        self.uploadList = []
+        self.refreshFileInfo()
+        self.hideBtn()
 
-if __name__ == "__main__":
-    tool = SMMSTool()
+    def refreshFileInfo(self):
+        if len(self.uploadList) == 0:
+            self.fileinfo.setText('')
+        elif len(self.uploadList) == 1:
+            self.fileinfo.setText(self.uploadList[0])
+        else:
+            self.fileinfo.setText("选择了" + len(self.uploadList).__str__() + "个文件")
 
-    clearCode, clearMsg = tool.clear()
-    print(clearMsg)
+    def hideBtn(self):
+        self.removeBtn.hide()
+        self.uploadBtn.hide()
 
-    filepath1 = r"D:\test.jpg"
-    uploadStatus, uploadMsg = tool.upload(filepath1)
-    print(uploadMsg)
+    def showBtn(self):
+        self.removeBtn.show()
+        self.uploadBtn.show()
 
-    for url in tool.urlList:
-        print("url:" + url.url + "\n" + "delete:" + url.delete + "\n\n")
+    def browse(self):
+        filepaths = QFileDialog.getOpenFileNames(self, "选择文件", "C:/")[0]
+        for filepath in filepaths:
+            self.uploadList.append(filepath)
+        self.showBtn()
+        self.refreshFileInfo()
 
-    filepath2 = r"D:\test2.jpg"
-    uploadStatus, uploadMsg = tool.upload(filepath2)
-    print(uploadMsg)
+    def showUrl(self):
+        # self.hasResult = True
+        # self.tool.urlList = [ImageUrl('test1.jpg', 'https://test1.jpg', 'https://deletetest1.jpg'),
+        #                      ImageUrl('test2.jpg', 'https://test2.jpg', 'https://deletetest2.jpg'),
+        #                      ImageUrl('test3.jpg', 'https://test3.jpg', 'https://deletetest3.jpg'), ]
 
-    for url in tool.urlList:
-        print("url:" + url.url + "\n" + "delete:" + url.delete + "\n\n")
+        if self.hasResult:
+            self.list.clear()
+            sender = self.sender()
 
-    historyStatus, historyData = tool.history()
-    if historyStatus == -1 or -2:
-        print(historyData)
-    elif historyStatus == 0:
-        for data in historyData:
-            print("url:" + data['url'] + "\n" + "delete:" + data['delete'] + "\n\n")
+            if sender.text() == 'url':
+                for url in self.tool.urlList:
+                    self.list.addItem(QListWidgetItem(url.url))
+            elif sender.text() == 'markdown':
+                for url in self.tool.urlList:
+                    self.list.addItem(QListWidgetItem("![" + url.name + "](" + url.url + ")"))
+            elif sender.text() == 'delete':
+                for url in self.tool.urlList:
+                    self.list.addItem(QListWidgetItem(url.delete))
+        else:
+            QMessageBox.question(self, "警告", "不存在结果", QMessageBox.Yes, QMessageBox.Yes)
+
+    def copyToClipboard(self, item):
+        print(item.text())
+        r = Tk()
+        r.withdraw()
+        r.clipboard_clear()
+        r.clipboard_append(item.text())
+        r.destroy()
 
 
-
-
-
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    smmsToolGUI = SMMSToolGUI()
+    sys.exit(app.exec_())
